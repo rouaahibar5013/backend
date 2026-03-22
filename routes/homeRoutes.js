4b3bdb44fc914d9d6f494ff2fff7722328982e76
Hizem-Ines <hizemm.ines@gmail.com>
2026-03-22 13:26:13 +0100

    Revert "Merge branch 'main' into add-review"
    This reverts commit 19ca1287c2ec9780d306d54eb6e8ce9ddb2645a5, reversing
    changes made to 712c781079e8680e635511bad59aaef08c495c61.

--- routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getHomeData } from "../controllers/homeController.js";
-
-const router = express.Router();
-
-router.get("/", getHomeData);
-
-export default router;
